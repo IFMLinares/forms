b28440438de5e97078c5e5cc9729da2c57b3612b
Miguel Linares <titolfalcon@gmail.com>
2021-03-29 18:46:09 -0400

    Primerios cambios

--- apps/formApp/urls.py
+++ apps/formApp/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from .views import (
-    test
+    Index,
+    Proteccion,
+    Insercion,
+    Examen,
+    Documentacion
 )
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', test, name='test'),
+    path('', Index.as_view(), name='index'),
+    path('proteccion/', Proteccion.as_view(), name='proteccion'),
+    path('insercion/', Insercion.as_view(), name='insercion'),
+    path('examen/', Examen.as_view(), name='examen'),
+    path('documentacion/', Documentacion.as_view(), name='documentacion'),
 ]
 
--- apps/formApp/views.py
+++ apps/formApp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 
+class Index(TemplateView):
+    template_name = 'index.html'
 
-def test(request):
-    return render(request, 'index.html')   
+class Proteccion(TemplateView):
+    template_name = 'proteccion.html'
+
+class Insercion(TemplateView):
+    template_name = 'insercion.html'
+
+class Examen(TemplateView):
+    template_name = 'examen.html'
+
+class Documentacion(TemplateView):
+    template_name = 'documentacion.html'
